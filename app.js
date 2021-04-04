103eeff22d7411c4f4cf89ffc46105bd471a5c4e
Olya <oleatrik@gmail.com>
2021-04-05 00:52:16 +0300

    shows weather cast

---
+++ app.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const argv = process.argv
+const city = argv[2]
+async function printCurrentWeather(cityName) {
+    const OPEN_WEATHER__MAP_API = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName
+        + '&appid=***REMOVED***&units=metric&lang=ro';
+    try {
+        const response = await axios.get(OPEN_WEATHER__MAP_API)
+        let cast = response.data;
+        console.log(`În ${cast.name} se prognozează ${cast.weather[0].description}.` +
+            `\nTemperatura curentă este de ${Math.round(cast.main.temp)}°C.`);
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+
+printCurrentWeather(city);
