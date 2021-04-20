4362c8c9cbfa24ebbd12d4a1ce6dbc30b8f88cfc
Olya Tricolici <oleatrik@gmail.com>
2021-04-20 14:43:25 +0300

    fixes problem with diacrytics

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 import { printCurrentWeather, printWeatherFor7Days } from "./WeatherAPI.js";
-const city = process.argv[2];
+const city = encodeURI(process.argv[2]);
 
 const coords = await printCurrentWeather(city);
 printWeatherFor7Days(coords)
